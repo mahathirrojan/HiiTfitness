91c473608c31dd7c75f6a1091982168e3e578e9b
mahathirrojan <torbengalifreind@gmail.com>
2024-09-11 02:42:13 -0400

    Initial Commit

---
+++ HiiT fit/HiiT_fitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiiT_fitApp.swift
+//  HiiT fit
+//
+//  Created by Mahathir Rojan on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct HiiT_fitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
